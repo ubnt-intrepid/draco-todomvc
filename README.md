3b63bd6dc9d6c77b588d69fcf2cd7121f1522d81
Yusuke Sasaki <ubnt-intrepid@users.noreply.github.com>
2020-09-12 15:01:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# `draco-webpack-starter`
+
+An **unofficial** template for Web apps with [`draco`] and Webpack.
+
+Inspired by [`seed-quickstart-webpack`](https://github.com/seed-rs/seed-quickstart-webpack).
+
+<!-- links -->
+[`draco`]: https://github.com/seed-rs/seed-quickstart-webpack
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+
+const appDir = path.resolve(__dirname, 'app');
+const srcDir = path.resolve(__dirname, 'src');
+const distDir = path.resolve(__dirname, 'dist');
+
+module.exports = (env, argv) => {
+    return {
+        mode: 'development',
+        entry: {
+            app: path.resolve(srcDir, 'index.js'),
+        },
+        output: {
+            publicPath: '/',
+            path: distDir,
+            filename: '[name].[contenthash].js',
+        },
+        devServer: {
+            contentBase: distDir,
+            host: '0.0.0.0',
+            port: 8080,
+        },
+        plugins: [
+            new WasmPackPlugin({
+                crateDirectory: appDir,
+                extraArgs: '--no-typescript',
+                outName: 'index',
+                outDir: path.resolve(srcDir, 'app'),
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: path.resolve(srcDir, 'index.html'),
+            }),
+        ],
+    };
+};
